e168bc58f95abe9efa6f50bddeac05cb4b30d112
Claus Höfele <claus@claushoefele.com>
2016-10-21 22:20:31 +0200

    Add public init and == methods

--- Sources/AlexaSkillsKit/Request.swift
+++ Sources/AlexaSkillsKit/Request.swift
@@ -1,26 +1,65 @@
 import Foundation
 
-public struct LaunchRequest {
+public struct LaunchRequest: Equatable {
     public var request: Request
+    
+    public init(request: Request) {
+        self.request = request
+    }
+    
+    public static func ==(lhs: LaunchRequest, rhs: LaunchRequest) -> Bool {
+        return lhs.request == rhs.request
+    }
 }
 
-public struct Request {
+public struct Request: Equatable {
     public var requestId: String
     public var timestamp: Date
     public var locale: Locale
+    
+    public init(requestId: String, timestamp: Date, locale: Locale) {
+        self.requestId = requestId
+        self.timestamp = timestamp
+        self.locale = locale
+    }
+    
+    public static func ==(lhs: Request, rhs: Request) -> Bool {
+        return lhs.requestId == rhs.requestId && lhs.timestamp == rhs.timestamp && lhs.locale == rhs.locale
+    }
 }
 
-public struct IntentRequest {
+public struct IntentRequest: Equatable {
     public var request: Request
     public var intent: Intent
+    
+    public init(request: Request, intent: Intent) {
+        self.request = request
+        self.intent = intent
+    }
+    
+    public static func ==(lhs: IntentRequest, rhs: IntentRequest) -> Bool {
+        return lhs.request == rhs.request && lhs.intent == rhs.intent
+    }
 }
 
-public struct Intent {
+public struct Intent: Equatable {
     public var name: String
     public var slots: [String: Slot]
+    
+    public init(name: String, slots: [String: Slot]) {
+        self.name = name
+        self.slots = slots
+    }
+    
+    public static func ==(lhs: Intent, rhs: Intent) -> Bool {
+        return lhs.name == rhs.name && lhs.slots == rhs.slots
+    }
 }
 
 public struct Slot: Equatable {
+    public var name: String
+    public var value: String?
+
     public init(name: String, value: String? = nil) {
         self.name = name
         self.value = value
@@ -29,14 +68,20 @@ public struct Slot: Equatable {
     public static func ==(lhs: Slot, rhs: Slot) -> Bool {
         return lhs.name == rhs.name && lhs.value == rhs.value
     }
-
-    public var name: String
-    public var value: String?
 }
 
-public struct SessionEndedRequest {
+public struct SessionEndedRequest: Equatable {
     public var request: Request
     public var reason: Reason
+    
+    public init(request: Request, reason: Reason) {
+        self.request = request
+        self.reason = reason
+    }
+    
+    public static func ==(lhs: SessionEndedRequest, rhs: SessionEndedRequest) -> Bool {
+        return lhs.request == rhs.request && lhs.reason == rhs.reason
+    }
 }
 
 public enum Reason: Equatable {
@@ -58,6 +103,11 @@ public enum Reason: Equatable {
 public struct Error: Equatable {
     public var type: ErrorType
     public var message: String
+    
+    public init(type: ErrorType, message: String) {
+        self.type = type
+        self.message = message
+    }
     
     public static func ==(lhs: Error, rhs: Error) -> Bool {
         return lhs.type == rhs.type && lhs.message == rhs.message
--- Tests/AlexaSkillsKitTests/RequestTests.swift
+++ Tests/AlexaSkillsKitTests/RequestTests.swift
@@ -2,12 +2,36 @@ import AlexaSkillsKit
 import XCTest
 
 class RequestTests: XCTestCase {
-    func testSlot() {
+    func testLaunchRequest() {
+        let date = Date()
+        let locale = Locale(identifier: "identifier")
+        let request = Request(requestId: "requestId", timestamp: date, locale: locale)
+        XCTAssertEqual(request, request)
+        XCTAssertEqual(request, Request(requestId: "requestId", timestamp: date, locale: locale))
+        
+        XCTAssertNotEqual(request, Request(requestId: "x", timestamp: date, locale: locale))
+    }
+    
+    func testIntentRequest() {
+        let request0 = Request(requestId: "requestId", timestamp: Date(), locale: Locale(identifier: "identifier"))
         let slot = Slot(name: "name", value: "value")
-        XCTAssertEqual(slot, slot)
-        XCTAssertEqual(slot, Slot(name: "name", value: "value"))
-        XCTAssertNotEqual(slot, Slot(name: "namex", value: "value"))
-        XCTAssertNotEqual(slot, Slot(name: "name", value: "valuex"))
-        XCTAssertNotEqual(slot, Slot(name: "namex"))
+        let intent = Intent(name: "name", slots: ["slot": slot])
+        let intentRequest = IntentRequest(request: request0, intent: intent)
+        XCTAssertEqual(intentRequest, intentRequest)
+        XCTAssertEqual(intentRequest, IntentRequest(request: request0, intent: intent))
+        
+        let request1 = Request(requestId: "x", timestamp: Date(), locale: Locale(identifier: "x"))
+        XCTAssertNotEqual(intentRequest, IntentRequest(request: request1, intent: intent))
+    }
+    
+    func testSessionEndedRequest() {
+        let request0 = Request(requestId: "requestId", timestamp: Date(), locale: Locale(identifier: "identifier"))
+        let reason: Reason = .error(Error(type: .invalidResponse, message: "message"))
+        let sessionEndedRequest = SessionEndedRequest(request: request0, reason: reason)
+        XCTAssertEqual(sessionEndedRequest, sessionEndedRequest)
+        XCTAssertEqual(sessionEndedRequest, SessionEndedRequest(request: request0, reason: reason))
+
+        let request1 = Request(requestId: "x", timestamp: Date(), locale: Locale(identifier: "x"))
+        XCTAssertNotEqual(sessionEndedRequest, SessionEndedRequest(request: request1, reason: reason))
     }
 }
