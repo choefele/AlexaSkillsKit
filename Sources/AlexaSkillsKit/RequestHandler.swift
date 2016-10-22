5d2408b62ed70041fefe80921f32a2999637d1ae
Claus Höfele <claus@claushoefele.com>
2016-10-22 17:06:01 +0200

    Make API asynchronous

--- Sources/AlexaSkillsKit/RequestHandler.swift
+++ Sources/AlexaSkillsKit/RequestHandler.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 public protocol RequestHandler {
-    func handleLaunch(request: LaunchRequest) -> StandardResponse
-    func handleIntent(request: IntentRequest) -> StandardResponse
-    func handleSessionEnded(request: SessionEndedRequest)
+    func handleLaunch(request: LaunchRequest, next: @escaping (StandardResponse) -> ())
+    func handleIntent(request: IntentRequest, next: @escaping (StandardResponse) -> ())
+    func handleSessionEnded(request: SessionEndedRequest, next: @escaping () -> ())
 }
