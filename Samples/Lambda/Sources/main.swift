e6f05126e9cbc717cc6b200010823f1a8e89789c
Claus Höfele <claus@claushoefele.com>
2016-10-26 20:57:29 +0200

    Add script to build Lambda deployment package

--- Samples/Lambda/Sources/main.swift
+++ Samples/Lambda/Sources/main.swift
@@ -2,19 +2,21 @@ import Foundation
 import AlexaSkillsKit
 
 func echo(string: String) -> String {
+    let errorMessage = "{\"version\": \"1.0\", \"response\": {\"outputSpeech\": {\"type\": \"PlainText\", \"text\": \"%@\"}, \"shouldEndSession\": true }, \"sessionAttributes\": {}}"
+    
     guard let data = string.data(using: .utf8),
         let requestParser = try? RequestParser(with: data),
         let _ = requestParser.parseRequestType() else {
-            return "error parsing input\n"
+            return String(format: errorMessage, "Error 1")
     }
     
-    let outputSpeech = OutputSpeech.plain(text: "text")
+    let outputSpeech = OutputSpeech.plain(text: "Test Swift on Lambda")
     let standardResponse = StandardResponse(outputSpeech: outputSpeech, shouldEndSession: true)
     let responseGenerator = ResponseGenerator(standardResponse: standardResponse)
     guard let jsonData = try? responseGenerator.generateJSON(options: .prettyPrinted) else {
-        return "error generating JSON\n"
+        return String(format: errorMessage, "Error 2")
     }
     
-    return String(data: jsonData, encoding: .utf8) ?? "error converting to String"
+    return String(data: jsonData, encoding: .utf8) ?? String(format: errorMessage, "Error 3")
 }
 readTransformPrint(transform:echo)
