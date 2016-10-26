3295f08b192f2b850b0ce6e0c70a63a87096623d
Claus Höfele <claus@claushoefele.com>
2016-10-26 22:34:14 +0200

    Simplify stdin/stdout handling

--- Samples/Lambda/Sources/main.swift
+++ Samples/Lambda/Sources/main.swift
@@ -1,22 +1,21 @@
 import Foundation
 import AlexaSkillsKit
 
-func echo(string: String) -> String {
-    let errorMessage = "{\"version\": \"1.0\", \"response\": {\"outputSpeech\": {\"type\": \"PlainText\", \"text\": \"%@\"}, \"shouldEndSession\": true }, \"sessionAttributes\": {}}"
-    
-    guard let data = string.data(using: .utf8),
-        let requestParser = try? RequestParser(with: data),
+func transform(fileHandle: FileHandle) -> Data {
+    guard let requestParser = try? RequestParser(fileHandle: fileHandle),
         let _ = requestParser.parseRequestType() else {
-            return String(format: errorMessage, "Error 1")
+            return "{\"version\": \"1.0\", \"response\": {\"outputSpeech\": {\"type\": \"PlainText\", \"text\": \"Error 1\"}, \"shouldEndSession\": true }}".data(using: .utf8)!
     }
-    
+
     let outputSpeech = OutputSpeech.plain(text: "Test Swift on Lambda")
     let standardResponse = StandardResponse(outputSpeech: outputSpeech, shouldEndSession: true)
     let responseGenerator = ResponseGenerator(standardResponse: standardResponse)
     guard let jsonData = try? responseGenerator.generateJSON(options: .prettyPrinted) else {
-        return String(format: errorMessage, "Error 2")
+        return "{\"version\": \"1.0\", \"response\": {\"outputSpeech\": {\"type\": \"PlainText\", \"text\": \"Error 2\"}, \"shouldEndSession\": true }}".data(using: .utf8)!
     }
     
-    return String(data: jsonData, encoding: .utf8) ?? String(format: errorMessage, "Error 3")
+    return jsonData
 }
-readTransformPrint(transform:echo)
+
+let result = transform(fileHandle: FileHandle.standardInput)
+FileHandle.standardOutput.write(result)
