8795d231c55c26a286a92caa0afe9cd810cd680b
Claus Höfele <claus@claushoefele.com>
2016-10-31 12:31:28 +0100

    Add additional reference documentation

--- Sources/AlexaSkillsKit/Request.swift
+++ Sources/AlexaSkillsKit/Request.swift
@@ -1,12 +1,39 @@
 import Foundation
 
+/// Provides additional context associated with a request that is maintained
+/// between request invocations.
+///
+/// Session attributes will survive individual request invocations if attributes
+/// are passed back in the response as `sessionAttributes` (see 
+/// `ResponseGenerator`) and the `shouldEndSession` flag is set to `false` (see
+/// `StandardResponse`).
+///
+/// The information in `user` and `application` properties is the same as in the
+/// `Context` type.
+///
+/// - SeeAlso: `StandardResponse.shouldEndSession`, `ResponseGenerator`
 public struct Session: Equatable {
+    /// A Bool value that indicates whether this session was created from
+    /// scratch for this request.
     public var isNew: Bool
+    /// Unique identifier for a session.
     public var sessionId: String
+    /// Application information.
     public var application: Application
+    /// Session attributes.
     public var attributes: [String: Any]
+    /// User information.
     public var user: User
     
+    /// Creates a new session with initial values.
+    ///
+    /// - Parameters:
+    ///   - isNew: A Bool value that indicates whether this session was created from
+    ///            scratch for this request.
+    ///   - sessionId: Unique identifier for a session.
+    ///   - application: Application information.
+    ///   - attributes: Session attributes.
+    ///   - user: User information.
     public init(isNew: Bool, sessionId: String, application: Application, attributes: [String: Any], user: User) {
         self.isNew = isNew
         self.sessionId = sessionId
@@ -15,6 +42,14 @@ public struct Session: Equatable {
         self.user = user
     }
     
+    /// Returns a Boolean value indicating whether two values are equal.
+    ///
+    /// Equality is the inverse of inequality. For any values `a` and `b`,
+    /// `a == b` implies that `a != b` is `false`.
+    ///
+    /// - Parameters:
+    ///   - lhs: A value to compare.
+    ///   - rhs: Another value to compare.
     public static func ==(lhs: Session, rhs: Session) -> Bool {
         return lhs.isNew == rhs.isNew &&
             lhs.sessionId == rhs.sessionId
@@ -28,6 +63,14 @@ public struct Application: Equatable {
         self.applicationId = applicationId
     }
     
+    /// Returns a Boolean value indicating whether two values are equal.
+    ///
+    /// Equality is the inverse of inequality. For any values `a` and `b`,
+    /// `a == b` implies that `a != b` is `false`.
+    ///
+    /// - Parameters:
+    ///   - lhs: A value to compare.
+    ///   - rhs: Another value to compare.
     public static func ==(lhs: Application, rhs: Application) -> Bool {
         return lhs.applicationId == rhs.applicationId
     }
@@ -42,6 +85,14 @@ public struct User: Equatable {
         self.accessToken = accessToken
     }
     
+    /// Returns a Boolean value indicating whether two values are equal.
+    ///
+    /// Equality is the inverse of inequality. For any values `a` and `b`,
+    /// `a == b` implies that `a != b` is `false`.
+    ///
+    /// - Parameters:
+    ///   - lhs: A value to compare.
+    ///   - rhs: Another value to compare.
     public static func ==(lhs: User, rhs: User) -> Bool {
         return lhs.userId == rhs.userId &&
             lhs.accessToken == rhs.accessToken
@@ -55,6 +106,14 @@ public struct LaunchRequest: Equatable {
         self.request = request
     }
     
+    /// Returns a Boolean value indicating whether two values are equal.
+    ///
+    /// Equality is the inverse of inequality. For any values `a` and `b`,
+    /// `a == b` implies that `a != b` is `false`.
+    ///
+    /// - Parameters:
+    ///   - lhs: A value to compare.
+    ///   - rhs: Another value to compare.
     public static func ==(lhs: LaunchRequest, rhs: LaunchRequest) -> Bool {
         return lhs.request == rhs.request
     }
@@ -71,6 +130,14 @@ public struct Request: Equatable {
         self.locale = locale
     }
     
+    /// Returns a Boolean value indicating whether two values are equal.
+    ///
+    /// Equality is the inverse of inequality. For any values `a` and `b`,
+    /// `a == b` implies that `a != b` is `false`.
+    ///
+    /// - Parameters:
+    ///   - lhs: A value to compare.
+    ///   - rhs: Another value to compare.
     public static func ==(lhs: Request, rhs: Request) -> Bool {
         return lhs.requestId == rhs.requestId &&
             lhs.timestamp == rhs.timestamp &&
@@ -87,20 +154,37 @@ public struct IntentRequest: Equatable {
         self.intent = intent
     }
     
+    /// Returns a Boolean value indicating whether two values are equal.
+    ///
+    /// Equality is the inverse of inequality. For any values `a` and `b`,
+    /// `a == b` implies that `a != b` is `false`.
+    ///
+    /// - Parameters:
+    ///   - lhs: A value to compare.
+    ///   - rhs: Another value to compare.
     public static func ==(lhs: IntentRequest, rhs: IntentRequest) -> Bool {
         return lhs.request == rhs.request && lhs.intent == rhs.intent
     }
 }
 
 public struct Intent: Equatable {
+
     public var name: String
     public var slots: [String: Slot]
-    
+
     public init(name: String, slots: [String: Slot] = [:]) {
         self.name = name
         self.slots = slots
     }
-    
+
+    /// Returns a Boolean value indicating whether two values are equal.
+    ///
+    /// Equality is the inverse of inequality. For any values `a` and `b`,
+    /// `a == b` implies that `a != b` is `false`.
+    ///
+    /// - Parameters:
+    ///   - lhs: A value to compare.
+    ///   - rhs: Another value to compare.
     public static func ==(lhs: Intent, rhs: Intent) -> Bool {
         return lhs.name == rhs.name && lhs.slots == rhs.slots
     }
@@ -133,6 +217,14 @@ public struct Slot: Equatable {
         self.value = value
     }
     
+    /// Returns a Boolean value indicating whether two values are equal.
+    ///
+    /// Equality is the inverse of inequality. For any values `a` and `b`,
+    /// `a == b` implies that `a != b` is `false`.
+    ///
+    /// - Parameters:
+    ///   - lhs: A value to compare.
+    ///   - rhs: Another value to compare.
     public static func ==(lhs: Slot, rhs: Slot) -> Bool {
         return lhs.name == rhs.name && lhs.value == rhs.value
     }
@@ -147,6 +239,14 @@ public struct SessionEndedRequest: Equatable {
         self.reason = reason
     }
     
+    /// Returns a Boolean value indicating whether two values are equal.
+    ///
+    /// Equality is the inverse of inequality. For any values `a` and `b`,
+    /// `a == b` implies that `a != b` is `false`.
+    ///
+    /// - Parameters:
+    ///   - lhs: A value to compare.
+    ///   - rhs: Another value to compare.
     public static func ==(lhs: SessionEndedRequest, rhs: SessionEndedRequest) -> Bool {
         return lhs.request == rhs.request &&
             lhs.reason == rhs.reason
@@ -159,6 +259,14 @@ public enum Reason: Equatable {
     case error(Error)
     case exceededMaxReprompts
     
+    /// Returns a Boolean value indicating whether two values are equal.
+    ///
+    /// Equality is the inverse of inequality. For any values `a` and `b`,
+    /// `a == b` implies that `a != b` is `false`.
+    ///
+    /// - Parameters:
+    ///   - lhs: A value to compare.
+    ///   - rhs: Another value to compare.
     public static func ==(lhs: Reason, rhs: Reason) -> Bool {
         switch (lhs, rhs) {
         case (.userInitiated, .userInitiated): return true
@@ -178,6 +286,14 @@ public struct Error: Equatable {
         self.message = message
     }
     
+    /// Returns a Boolean value indicating whether two values are equal.
+    ///
+    /// Equality is the inverse of inequality. For any values `a` and `b`,
+    /// `a == b` implies that `a != b` is `false`.
+    ///
+    /// - Parameters:
+    ///   - lhs: A value to compare.
+    ///   - rhs: Another value to compare.
     public static func ==(lhs: Error, rhs: Error) -> Bool {
         return lhs.type == rhs.type && lhs.message == rhs.message
     }
