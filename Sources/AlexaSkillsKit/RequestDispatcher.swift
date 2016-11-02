f81a7bc13ec4e63b79c0f0c70941e2ef89e64d29
Claus Höfele <claus@claushoefele.com>
2016-11-02 17:33:36 +0100

    First draft of RequestDispatcher

---
+++ Sources/AlexaSkillsKit/RequestDispatcher.swift
@@ -0,0 +1,46 @@
+import Foundation
+
+open class RequestDispatcher {
+    public let requestHandler: RequestHandler
+
+    public init(requestHandler: RequestHandler) {
+        self.requestHandler = requestHandler
+    }
+
+    open func dispatch(fileHandle: FileHandle) -> Data {
+        return dispatch(data: fileHandle.readDataToEndOfFile())
+    }
+
+    open func dispatch(data: Data) -> Data {
+        guard let requestParser = try? RequestParser(with: data),
+            let requestType = requestParser.parseRequestType() else {
+                return "{\"version\": \"1.0\", \"response\": {\"outputSpeech\": {\"type\": \"PlainText\", \"text\": \"Error parsing request\"}, \"shouldEndSession\": true }}".data(using: .utf8)!
+        }
+
+        var response = (standardResponse: StandardResponse(), sessionAttributes: [:])
+        switch requestType {
+        case .intent:
+            guard let intentRequest = requestParser.parseIntentRequest(),
+                let session = requestParser.parseSession() else {
+                return handleError("Error parsing intent")
+            }
+
+            requestHandler.handleIntent(request: intentRequest, session: session, next: { (standardResponse) in
+                response = (standardResponse, [:])
+            })
+        default:
+            response = (StandardResponse(), [:])
+        }
+
+        let responseGenerator = ResponseGenerator(standardResponse: response.standardResponse)
+        guard let jsonData = try? responseGenerator.generateJSON(options: .prettyPrinted) else {
+            return "{\"version\": \"1.0\", \"response\": {\"outputSpeech\": {\"type\": \"PlainText\", \"text\": \"Error generating response\"}, \"shouldEndSession\": true }}".data(using: .utf8)!
+        }
+        
+        return jsonData
+    }
+
+    func handleError(_ message: String) -> Data {
+        return "{\"version\": \"1.0\", \"response\": {\"outputSpeech\": {\"type\": \"PlainText\", \"text\": \"\(message)\"}, \"shouldEndSession\": true }}".data(using: .utf8)!
+    }
+}
