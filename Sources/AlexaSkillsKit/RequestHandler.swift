6f60483e948ba9551acacf8588a0fceb34919ac1
Claus Höfele <claus@claushoefele.com>
2016-11-02 17:33:36 +0100

    First draft of RequestDispatcher

--- Sources/AlexaSkillsKit/RequestHandler.swift
+++ Sources/AlexaSkillsKit/RequestHandler.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 public protocol RequestHandler {
-    func handleLaunch(request: LaunchRequest, next: @escaping (StandardResponse) -> ())
-    func handleIntent(request: IntentRequest, next: @escaping (StandardResponse) -> ())
-    func handleSessionEnded(request: SessionEndedRequest, next: @escaping () -> ())
+    func handleLaunch(request: LaunchRequest, session: Session, next: @escaping (StandardResponse) -> ())
+    func handleIntent(request: IntentRequest, session: Session, next: @escaping (StandardResponse) -> ())
+    func handleSessionEnded(request: SessionEndedRequest, session: Session, next: @escaping () -> ())
 }
