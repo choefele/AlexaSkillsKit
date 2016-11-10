c45237fe8122d0629e607c42044159ede3a5e152
Claus Höfele <claus@claushoefele.com>
2016-11-10 21:23:02 +0100

    Try out RequestDispatcher

--- Sample/Sources/AlexaSkill/AlexaSkillHandler.swift
+++ Sample/Sources/AlexaSkill/AlexaSkillHandler.swift
@@ -1,6 +1,19 @@
 import Foundation
 import AlexaSkillsKit
 
-class AlexaSkillHandler {
+public class AlexaSkillHandler : RequestHandler {
+    public init() {
+        
+    }
     
+    public func handleLaunch(request: LaunchRequest, session: Session, next: @escaping (StandardResponse) -> ()) {
+    }
+    
+    public func handleIntent(request: IntentRequest, session: Session, next: @escaping (StandardResponse) -> ()) {
+        
+    }
+    
+    public func handleSessionEnded(request: SessionEndedRequest, session: Session, next: @escaping () -> ()) {
+        
+    }
 } 
