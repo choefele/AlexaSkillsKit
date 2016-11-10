08378a4b2c6e24625ea6a7bddb5a31fca87a1181
Claus Höfele <claus@claushoefele.com>
2016-11-10 14:37:03 +0100

    Define additional target on macOS for debug server

--- Samples/Lambda/Package.swift
+++ Samples/Lambda/Package.swift
@@ -1,7 +1,19 @@
 import PackageDescription
 
-let package = Package(
-    name: "Lambda",
+var package = Package(
+    name: "AlexaSkill",
+    targets: [
+        Target(name: "AlexaSkill"),
+        Target(name: "Lambda", dependencies: ["AlexaSkill"])
+    ],
     dependencies: [
-        .Package(url: "https://github.com/choefele/AlexaSkillsKit", majorVersion: 0)
-    ])
+        .Package(url: "https://github.com/choefele/AlexaSkillsKit", majorVersion: 0),
+    ], 
+    exclude: ["Sources/Server"])
+
+#if os(macOS)
+package.targets.append(Target(name: "Server", dependencies: ["AlexaSkill"]))
+package.dependencies.append(.Package(url: "https://github.com/IBM-Swift/Kitura", majorVersion: 1, minor: 1))
+package.dependencies.append(.Package(url: "https://github.com/IBM-Swift/HeliumLogger", majorVersion: 1, minor: 1))
+package.exclude = []
+#endif
---
+++ Samples/Lambda/Sources/AlexaSkill/AlexaSkillHandler.swift
@@ -0,0 +1,6 @@
+import Foundation
+import AlexaSkillsKit
+
+class AlexaSkillHandler {
+    
+} 
