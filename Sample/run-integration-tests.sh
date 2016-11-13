7704b1ffe3e1c539bfaddf4227f7400f87338b6a
Claus Höfele <claus@claushoefele.com>
2016-11-13 21:00:56 +0100

    Simplify script

--- Sample/run-integration-tests.sh
+++ Sample/run-integration-tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 eval "$(aws ecr get-login --region us-east-1 --registry-id 137112412989)"
-docker run --rm -v "$(pwd):/lambda" -w /src 137112412989.dkr.ecr.us-east-1.amazonaws.com/amazonlinux:2016.09 /bin/bash -c 'LD_LIBRARY_PATH=/lambda/.build/lambda/native/LinuxLibraries /lambda/.build/lambda/native/Lambda < /lambda/session_start.json'
+docker run --rm -v "$(pwd):/lambda" -w /lambda 137112412989.dkr.ecr.us-east-1.amazonaws.com/amazonlinux:2016.09 /bin/bash -c 'LD_LIBRARY_PATH=.build/lambda/native/LinuxLibraries .build/lambda/native/Lambda < session_start.json'
