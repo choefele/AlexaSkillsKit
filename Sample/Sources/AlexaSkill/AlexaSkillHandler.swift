60aa96a90f5521644ba9ea2f751f5bc8561a86a0
Claus Höfele <claus@claushoefele.com>
2016-11-13 20:27:52 +0100

    Add simple response

--- Sample/Sources/AlexaSkill/AlexaSkillHandler.swift
+++ Sample/Sources/AlexaSkill/AlexaSkillHandler.swift
@@ -6,14 +6,21 @@ public class AlexaSkillHandler : RequestHandler {
     }
     
     public func handleLaunch(request: LaunchRequest, session: Session, next: @escaping (StandardResult) -> ()) {
-        next(.success(standardResponse: StandardResponse(), sessionAttributes: session.attributes))
+        let standardResponse = generateResponse(message: "Alexa Skill received launch request")
+        next(.success(standardResponse: standardResponse, sessionAttributes: session.attributes))
     }
     
     public func handleIntent(request: IntentRequest, session: Session, next: @escaping (StandardResult) -> ()) {
-        next(.success(standardResponse: StandardResponse(), sessionAttributes: session.attributes))
+        let standardResponse = generateResponse(message: "Alexa Skill received intent \(request.intent.name)")
+        next(.success(standardResponse: standardResponse, sessionAttributes: session.attributes))
     }
     
     public func handleSessionEnded(request: SessionEndedRequest, session: Session, next: @escaping (VoidResult) -> ()) {
         next(.success())
+    }
+    
+    func generateResponse(message: String) -> StandardResponse {
+        let outputSpeech = OutputSpeech.plain(text: message)
+        return StandardResponse(outputSpeech: outputSpeech)
     }
 }
