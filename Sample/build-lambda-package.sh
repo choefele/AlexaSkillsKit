8397991f11ca498436ba993f662576b56a40bc35
Claus Höfele <claus@claushoefele.com>
2016-11-17 22:23:54 +0100

    Update to Swift 3.0.1

--- Sample/build-lambda-package.sh
+++ Sample/build-lambda-package.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+SWIFT_VERSION=3.0.1
+
 # Build Swift executable
 docker run \
     --rm \
     --volume "$(pwd):/app" \
     --workdir /app \
-    smithmicro/swift:3.0 \
+    smithmicro/swift:$SWIFT_VERSION \
     swift build -c release --build-path .build/native
 
 # Copy libraries necessary to run Swift executable
@@ -14,10 +16,10 @@ docker run \
     --rm \
     --volume "$(pwd):/app" \
     --workdir /app \
-    smithmicro/swift:3.0 \
+    smithmicro/swift:$SWIFT_VERSION \
     /bin/bash -c "ldd .build/native/release/Lambda | grep so | sed -e '/^[^\t]/ d' | sed -e 's/\t//' | sed -e 's/.*=..//' | sed -e 's/ (0.*)//' | xargs -i% cp % .build/lambda/native/LinuxLibraries"
 
-# Zip Swift executable, libraries and shim
+# Zip Swift executable, libraries and Node.js shim
 cp Shim/index.js .build/lambda
 cp .build/native/release/Lambda .build/lambda/native
 cd .build/lambda
