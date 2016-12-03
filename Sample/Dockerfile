e417dc4bf8adc4efacc25716923ba9d99ec4adc3
Claus Höfele <claus@claushoefele.com>
2016-12-03 14:50:08 +0100

    Remove sample
    Sample now lives at https://github.com/choefele/swift-lambda-app

--- Sample/Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM amazonlinux:2016.09
-MAINTAINER Claus
-
-RUN curl --silent --location https://rpm.nodesource.com/setup_4.x | bash - \
-  && yum install -y nodejs
--- Sample/run-integration-tests.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker build -t lambda .
-docker run --rm -v "$(pwd):/app" -w /app/.build/lambda lambda node -e 'var fs = require("fs");require("./").handler(JSON.parse(fs.readFileSync("../../session_start.json", "utf8")), {}, function(e, r) {if (e) {console.error(e);} else {console.log(r);}});'
